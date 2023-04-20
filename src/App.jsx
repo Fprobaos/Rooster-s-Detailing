98dfd306c6a84530ebc4f3545f80e047a27ecf41
Fprobaos <probaosfelipe@gmail.com>
2023-04-20 17:12:03 -0300

    Creando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ItemListContainer from './ItemListContainer/ItemListContainer'
+
+function App() {
+  
+
+  return(
+
+    <ItemListContainer />
+  )
+}
+
+export default App
